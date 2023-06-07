abbcf05ac92dd3f4090b4e9a03b3f99e75db35fa
moussaahmadou <96231307+moussaahmadou@users.noreply.github.com>
2023-06-07 19:10:54 +0100

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return "welcome Moussa DevOps Project";
 	}
 
 	public static void main(String[] args) {
